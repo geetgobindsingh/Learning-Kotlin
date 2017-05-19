8e7c37d5695b3022e855d0a45df4bc67e57e208a
Geet Gobind Singh <geetgobindsingh@gmail.com>
2017-05-20 00:47:36 +0530

    Added creating object details of kotlin

--- src/begining/classes/Objects.kt
+++ src/begining/classes/Objects.kt
@@ -3,3 +3,20 @@ package begining.classes
 /**
  * Created by Geet on 20-05-2017.
  */
+//Using `object` keyword implementing Singleton Pattern
+// We can create objects without initialising the classes
+object Global {
+    val PI :Int =  3.14
+}
+//endregion
+
+//region Main Function
+fun main(args: Array<String>) {
+    println(Global.PI)
+    //creating objects expression format
+    var myObject = object {
+        val daysInAWeek : Int = 7
+    }
+    println(myObject.daysInAWeek)
+}
+//endregion
